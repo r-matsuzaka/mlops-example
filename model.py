24c20aff7110397ceeec5ca4609434e025a73e1b
r-matsuzaka <matsuzakaryo@gmail.com>
2021-10-31 21:56:28 +0900

    feat:add package

---
+++ model.py
@@ -0,0 +1,19 @@
+import pickle
+
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+
+df = pd.read_csv(
+    "https://github.com/bgweber/Twitch/raw/master/Recommendations/games-expand.csv"
+)
+x = df.drop(["label"], axis=1)
+y = df["label"]
+
+model = LogisticRegression()
+model.fit(x, y)
+
+
+pickle.dump(model, open("logit.pkl", "wb"))
+
+model = pickle.load(open("logit.pkl", "rb"))
+model.predict_proba(x)
