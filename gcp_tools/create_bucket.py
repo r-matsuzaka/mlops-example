f00904dfa51e28fb2257336b8dc5ebe775798dc1
r-matsuzaka <matsuzakaryo@gmail.com>
2021-11-03 17:38:29 +0900

    feat:add code

---
+++ gcp_tools/create_bucket.py
@@ -0,0 +1,8 @@
+from google.cloud import storage
+
+bucket_name = "ml_model_store"
+storage_client = storage.Client()
+storage_client.create_bucket(bucket_name)
+
+for bucket in storage_client.list_buckets():
+    print(bucket.name) 
